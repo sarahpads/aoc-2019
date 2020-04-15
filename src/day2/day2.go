dc7e7e5a2123db78904c654c6585c5b8ad9a4d80
Sarah <sarahpads@gmail.com>
2020-04-14 22:22:23 -0300

    day-2: part 2

--- src/day2/day2.go
+++ src/day2/day2.go
@@ -7,8 +7,6 @@ import (
 
 type Opcode int
 
-var input = utils.GetInput("src/day2/input.txt")
-
 const (
 	ADD  Opcode = 1
 	MULT Opcode = 2
@@ -16,17 +14,38 @@ const (
 )
 
 func main() {
+	var input = utils.GetInput("src/day2/input.txt")
+
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			attempt := make([]int, len(input))
+			copy(attempt, input)
+			attempt[1] = noun
+			attempt[2] = verb
+
+			output := getOutput(attempt)
+
+			if output == 19690720 {
+				fmt.Println(output)
+				fmt.Println(100*noun + verb)
+				break
+			}
+		}
+	}
+}
+
+func getOutput(input []int) int {
 inputLoop:
 	for i := 0; i < len(input); i += 4 {
 		var opcode = Opcode(input[i])
 
 		switch opcode {
 		case ADD:
-			aValue, bValue, cValue := getValues(i)
+			aValue, bValue, cValue := getValues(input, i)
 			input[cValue] = aValue + bValue
 
 		case MULT:
-			aValue, bValue, cValue := getValues(i)
+			aValue, bValue, cValue := getValues(input, i)
 			input[cValue] = aValue * bValue
 
 		case STOP:
@@ -37,10 +56,10 @@ inputLoop:
 		}
 	}
 
-	fmt.Println(input[0])
+	return input[0]
 }
 
-func getValues(index int) (int, int, int) {
+func getValues(input []int, index int) (int, int, int) {
 	var a = input[index+1]
 	var b = input[index+2]
 	var aValue = input[a]
